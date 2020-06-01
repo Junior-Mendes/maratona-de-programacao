7b5d0ba1b2ff4e1249439499c6bc909a453ea2f3
Junior-Mendes <63074320+Junior-Mendes@users.noreply.github.com>
2020-06-01 09:39:03 -0300

    Exercício 1014 URI

---
+++ 1014.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        int distancia = input.nextInt();
+        double ccasto = input.nextDouble();
+
+        double mediaccasto = distancia / ccasto;
+        System.out.printf("%.3f km/l%n", mediaccasto);
+
+    }
+}
